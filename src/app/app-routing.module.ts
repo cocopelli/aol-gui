99ba97a915381c39d8d8b98985b157fad1203ab2
ooztas <oztas@gmx.net>
2018-01-28 12:03:11 +0100

    add Home component and routing modul

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {SongListComponent} from './song-list/song-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'songs',
+    component: SongListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
