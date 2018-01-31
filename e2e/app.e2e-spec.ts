4404963bf919adfcb795bd79a13584145a6820d2
ooztas <oztas@gmx.net>
2018-01-31 10:46:13 +0100

    first Tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('aol-gui App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to aol!');
+    expect(page.getParagraphText()).toEqual('Willkommen Freunde der "guten" Musik');
   });
 });
